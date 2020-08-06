84f83b35a0970baf224a50c7df3e39ee6f263a2c
Владислав Чурсин <vchursin1@mail.ru>
2020-08-06 15:43:29 +0400

    Main commit

---
+++ src/App.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+
+export default function App() {
+
+const [state, setValue] = React.useState({
+  columns: [
+    { title: 'ItemId', field: 'iteM_KEY' },
+    { title: 'Value', field: 'iteM_VALUE' }
+  ],
+  data: [],
+  catalogId: 3
+});
+
+  React.useEffect(() => {
+    const getData = async () => {
+      await fetch("http://localhost/Swagger/api/handbook/" + state.catalogId)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('getData resolve', result);
+          setValue({...state, data: result});
+        },
+        (error) => {
+          console.log('getData error');
+        }
+      );
+    };
+    getData();
+  }, []);
+
+  return (
+    <MaterialTable
+      title="Editable Example"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: (newData) =>
+          new Promise((resolve) => {
+            newData.lisT_ID = state.catalogId;
+            fetch("http://localhost/Swagger/api/handbook/", {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+              },
+              body: JSON.stringify(newData)
+            })
+            .then(res => res.json())
+            .then(
+              (result) => {
+                resolve();
+                console.log('RowAdd resolve', result);
+
+                if (result.status &&
+                  (result.status != 200 || result.status != 204)) {
+                  alert('Ошибка сервера! Повторите попытку добавления данных!');
+                  return;
+                }
+
+                setValue((prevState) => {
+                  const data = [...prevState.data];
+                  data.push(result);
+                  return { ...prevState, data };
+                });
+              },
+              (error) => {
+                console.log('RowAdd error');
+              }
+            ); 
+          }),
+        onRowUpdate: (newData, oldData) =>
+          new Promise((resolve) => {
+            fetch("http://localhost/Swagger/api/handbook/", {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+              },
+              body: JSON.stringify(newData)
+            })
+            .then(res => res.json())
+            .then(
+              (result) => {
+                resolve();
+                console.log('RowUpdate resolve', result);
+
+                if (result.status &&
+                  (result.status != 200 || result.status != 204)) {
+                  alert('Ошибка сервера! Повторите попытку изменения данных!');
+                  return;
+                }
+
+                if (oldData) {
+                  setValue((prevState) => {
+                    const data = [...prevState.data];
+                    data[data.indexOf(oldData)] = result;
+                    return { ...prevState, data };
+                  });
+                }
+              },
+              (error) => {
+                console.log('RowUpdate error');
+              }
+            );
+          }),
+        onRowDelete: (oldData) =>
+          new Promise((resolve) => {
+            fetch("http://localhost/Swagger/api/handbook/" + oldData.iteM_KEY, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'text/html;charset=utf-8'
+              },
+              body: JSON.stringify(oldData.iteM_KEY)
+            })
+            .then(res => res.json())
+            .then(
+              (result) => {
+                resolve();
+                console.log('RowDelete resolve', result);
+
+                if (result.status &&
+                  (result.status != 200 || result.status != 204)) {
+                  alert('Ошибка сервера! Повторите попытку удаления данных!');
+                  return;
+                }
+
+                setValue((prevState) => {
+                  const data = [...prevState.data];
+                  data.splice(data.indexOf(result), 1);
+                  return { ...prevState, data };
+                });
+              },
+              (error) => {
+                console.log('RowDelete error');
+              }
+            );
+          }),
+      }}
+    />
+  );
+}
